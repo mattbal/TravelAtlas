58cf5f247e89658a723cb01d5d51d25cd395c585
matt <matt@aozidesign.com>
2021-04-27 13:57:32 -0700

    Added navigation stack and new npm packages

--- TravelAtlas/View/home.js
+++ TravelAtlas/View/home.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: '24px', fontWeight: 'bold' }}>Test Home Screen</Text>
     </View>
   );
 };
