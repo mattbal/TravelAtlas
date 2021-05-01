3bef363579f17e9fcdf318c76ead3ce6b54b49a0
Tom Arad <tom.arad@wsu.edu>
2021-05-01 15:04:02 -0700

    Homepage Layout

--- TravelAtlas/App.js
+++ TravelAtlas/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import TabBar from './components/TabBar';
 
--- TravelAtlas/View/home.js
+++ TravelAtlas/View/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageBackground, StyleSheet, SafeAreaView, Button} from 'react-native';
 import styled from 'styled-components/native';
+
+import img from "../assets/flamingo-bg.jpg"
 
 const Body = styled.View`
   flex: 1;
@@ -11,9 +13,46 @@ const Body = styled.View`
 const Home = () => {
   return (
     <Body>
-      <Text value='Test Home Screen' />
+      <Screen>
+        <Background></Background>
+        <Wrapper>
+          <Button title="Signup">  
+          </Button>
+          <Button title="Login">  
+          </Button>
+        </Wrapper>
+      </Screen>
     </Body>
   );
 };
+
+const Background = styled.View`
+    flex-grow: 1;
+    border: 1px solid #000;
+    background-image: url(${img});
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+
+    `;
+
+const BackgroundColor = styled.View`
+    flex: 1;
+    background: "blue";
+    `;
+
+const Wrapper = styled.View`
+    display: flex;
+    flex-direction: column;
+    padding: 27px 18px 18px;
+    `;
+
+const Screen = styled.View`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+`;
 
 export default Home;
