b1886022fd2327ccc9088031a69e5229bf5473ae
mattbal <55065240+mattbal@users.noreply.github.com>
2021-05-02 14:35:44 -0700

    Revert "Feature/add redux fonts"

--- TravelAtlas/App.js
+++ TravelAtlas/App.js
@@ -1,26 +1,21 @@
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import TabBar from './components/TabBar';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_700Bold,
-} from '@expo-google-fonts/inter';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  let [fontsLoaded, error] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_700Bold,
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
-  return <TabBar />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- TravelAtlas/View/signup.js
+++ TravelAtlas/View/signup.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-const Signup = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
-};
-
-export default Signup;
+// placeholder
